fd89b43dd345559ee07d6c00d717f58ab851c7ff
“nozomi-yannai” <n.yannai.oc@gmail.com>
2024-04-07 20:56:35 +0900

    addressの変更４

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,5 +1,5 @@
 class Address < ApplicationRecord
     has_many :orders
-    belongs_to :customers
-    belongs_to :addressable, polymorphic: true
+    belongs_to :customers , class_name: "adrresses" , foreign_key: "customer_id"
+    belongs_to :addressable
 end
