702e31f3b575a0b621aa0015ff44895fbb6a2d17
“nozomi-yannai” <n.yannai.oc@gmail.com>
2024-04-07 21:12:35 +0900

    ５

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,5 +1,5 @@
 class Customer < ApplicationRecord
     has_many :orders
-    has_many :addresses , class_name: "addresses" , foreign_key: "addressable_id"
+    belongs_to :addresses , class_name: "addresses" , foreign_key: "addressable_id"
 end
   
